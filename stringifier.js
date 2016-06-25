87a377d3128a276fc1341322b3bc96373493cd92
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2016-06-25 11:33:42 -0400

    working so far... will refactor

--- stringifier.js
+++ stringifier.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const fs = require('fs');
 
 var input = '',
   stdin = process.stdin;
@@ -10,11 +11,36 @@ stdin.on('data', (chunk) => {
 });
 
 stdin.on('end', () => {
-  var inputObj = JSON.parse(input);
+  var inputJSON = JSON.parse(input);
 
-  var inputStr = stringifyObject(inputObj); // TODO: not necessarly an object
-  console.log(inputStr);
+  var homeMadeJsonStart = new Date().getTime();
+  var homeMadeJson = stringify(inputJSON);
+  var homeMadeJsonStop = new Date().getTime();
+
+  var normalJsonStart = new Date().getTime();
+  var normalJson = JSON.stringify(inputJSON);
+  var normalJsonStop = new Date().getTime();
+
+  fs.writeFileSync('normal.json', normalJson, 'utf8');
+  fs.writeFileSync('homeMade.json', homeMadeJson, 'utf8'); // if `diff normal.json homeMade.json` is empty, it worked
+  console.log(`HomeMade JSON: ${homeMadeJsonStop - homeMadeJsonStart}ms`);
+  console.log(`Normal JSON: ${normalJsonStop - normalJsonStart}ms`);
 });
+
+function stringify(json) {
+  let type = typeof json;
+
+  if (json === null)
+    return 'null';
+  else if (type === "number" || type === "boolean")
+    return json.toString();
+  else if (type === "string")
+    return '\"'.concat(escapeString(json), '\"');
+  else if (_.isArray(json))
+    return stringifyArray(json);
+  else if (type === "object")
+    return stringifyObject(json);
+}
 
 // TODO: put in its own file. This current one shall be for test usage
 function stringifyObject(obj) {
@@ -25,19 +51,7 @@ function stringifyObject(obj) {
   var keys = Object.keys(obj),
     keysLen = keys.length;
   keys.forEach((key, ind) => {
-    let val,
-      type = typeof obj[key];
-
-    if (obj[key] === null)
-      val = 'null';
-    else if (type === "number" || type === "boolean")
-      val = obj[key].toString();
-    else if (type === "string")
-      val = '\"'.concat(escapeString(obj[key]), '\"');
-    else if (_.isArray(obj[key]))
-      val = stringifyArray(obj[key]);
-    else if (type === "object")
-      val = stringifyObject(obj[key]);
+    let val = stringify(obj[key]);
 
     if (ind !== keysLen - 1) // no "," after the last prop
       val = val.concat(',');
@@ -49,15 +63,36 @@ function stringifyObject(obj) {
   return '{'.concat(jsonStr, '}'); // wrap object
 }
 
+function stringifyArray(arr) {
+  var jsonStr = '';
+
+  changeUndefinedToNull(arr);
+
+  var arrLen = arr.length;
+  arr.forEach((elem, ind) => {
+    let val = stringify(elem);
+
+    if (ind !== arrLen - 1) // no "," after the last prop
+      val = val.concat(',');
+
+    jsonStr = jsonStr.concat(val);
+  });
+
+  return '['.concat(jsonStr, ']'); // wrap object
+}
+
 function removeUndefinedProps(obj) {
   Object.keys(obj).forEach((key) => {
     if (obj[key] === undefined) delete obj[key];
   });
 }
 
-// TODO: change to "stringifyStruct" to be used for arrays/objects ?
-function stringifyArray(arr) {
-
+function changeUndefinedToNull(arr) {
+  arr.map((elem) => {
+    return elem === undefined ?
+      null:
+      elem;
+  });
 }
 
 function escapeString(str) {
